1fd4c49238c487ec61086fea2b2b974c7fc06c04
codakas <found.code.7@gmail.com>
2023-08-18 13:26:00 +0530

    Question on 18 Aug 2023

---
+++ question5.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(){
+
+	int fd1 = open("f1",O_CREAT);
+	int fd2 = open("f2",O_CREAT);
+	int fd3 = open("f3",O_CREAT);
+	int fd4 = open("f4",O_CREAT);
+	int fd5 = open("f5",O_CREAT);
+	
+	while(1);
+	return 0;
+}
