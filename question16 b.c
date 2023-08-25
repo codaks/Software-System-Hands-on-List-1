ec9bbd3084adb633b4e92983b5dff326ea410a17
codakas <found.code.7@gmail.com>
2023-08-25 10:00:47 +0530

    Question on 25 Aug 2023

---
+++ question16 b.c
@@ -0,0 +1,25 @@
+#include<unistd.h>
+#include<fcntl.h>
+#include<sys/types.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+        struct flock lock;
+        int fd;
+        fd = open("db", O_RDWR);
+        lock.l_type = F_WRLCK;
+        lock.l_whence= SEEK_SET;
+        lock.l_start=0;
+        lock.l_len=0;
+        lock.l_pid=getpid();
+        printf("Before entering into critical section\n");
+        fcntl(fd,F_SETLKW,&lock);
+        printf("Inside the critical section\n");
+        printf("Enter to Unlock\n");
+        getchar();
+        printf("Unclocked\n");
+        lock.l_type = F_UNLCK;
+        fcntl(fd,F_SETLK,&lock);
+        printf("finish\n");
+}
